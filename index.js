cc1d9a077731d5c08d8299c37a5a6cc802a84bf4
Jon Custer <joncuster@gmail.com>
2024-02-03 13:21:58 +0100

    fix: add index

---
+++ index.js
@@ -0,0 +1,10 @@
+const Grammar = require('./components/Grammar');
+const TextExpression = require('./components/TextExpression');
+const TextSymbol = require('./components/TextSymbol');
+
+module.exports = {
+  Grammar,
+  TextExpression,
+  TextSymbol,
+  default: Grammar,
+};
